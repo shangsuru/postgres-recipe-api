7106ab137792af95d13ba9375c5bddd406d6a858
shangsuru <helm.henry99@gmail.com>
2019-10-07 15:03:24 +0200

    scrape.js boilerplate

---
+++ db/scrape.js
@@ -0,0 +1,15 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+function generateSql(
+  title,
+  instructions,
+  recipe_img,
+  author,
+  prep_time,
+  ingredients // [{ingredient: ..., amount: ... }]
+) {
+
+}
+
+//function getRecipeData()
