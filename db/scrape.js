fd38cce8efe1cd3970ac9afa6e268da2a19066d7
shangsuru <helm.henry99@gmail.com>
2019-10-07 20:21:15 +0200

    function to gereate valid sql to insert recipe

--- db/scrape.js
+++ db/scrape.js
@@ -1,5 +1,8 @@
-const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
+const request = require("request");
+
+const FILE = "data.sql";
 
 function generateSql(
   title,
@@ -9,7 +12,30 @@ function generateSql(
   prep_time,
   ingredients // [{ingredient: ..., amount: ... }]
 ) {
+  fs.appendFileSync(
+    FILE,
+    `\ninsert into recipes (title, instructions,recipe_img, author, prep_time)
+     values ('${title}', '${instructions}', '${recipe_img}', '${author}', '${prep_time}');`
+  );
 
+  ingredients.forEach(elem => {
+    fs.appendFileSync(
+      FILE,
+      `\ninsert into ingredients values ('${title}', '${elem.ingredient}', '${elem.amount}');`
+    );
+  });
 }
+
+generateSql(
+  "Hot Dog",
+  "Get your dog and put it in the oven",
+  "CENSORED",
+  "anonymous",
+  "60",
+  [
+    { ingredient: "dog", amount: "1" },
+    { ingredient: "sauce", amount: "1 litre" }
+  ]
+);
 
 //function getRecipeData()
