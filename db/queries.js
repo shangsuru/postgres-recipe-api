ea504787eeb27f788a9f261c983b1b49382a83af
shangsuru <helm.henry99@gmail.com>
2019-10-11 09:58:08 +0200

    altered routes

--- db/queries.js
+++ db/queries.js
@@ -1,60 +1,72 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const Pool = require("pg").Pool;
+const Pool = require('pg').Pool
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-});
+})
 
 const getRecipes = (request, response) => {
-  const query = request.query.q;
+  const query = request.query.q
   pool.query(
-    "select recipe_name, rating, recipe_img, prep_time from recipes where recipe_name like $1",
-    ["%" + query + "%"],
+    'select recipe_name, rating, recipe_img, prep_time from recipes where recipe_name like $1',
+    ['%' + query + '%'],
     (error, results) => {
-      if (!error) {
-      response.status(200).json(results.rows);
+      if (error) {
+        console.log(error)
+      } else {
+        response.status(200).json(results.rows)
+      }
     }
-  );
-};
+  )
+}
 
 const getRecipeDetails = (request, response) => {
-  const title = request.params.title;
+  const title = request.params.title
   pool.query(
-    "select * from recipes where recipe_name = $1",
+    'select * from recipes where recipe_name = $1',
     [title],
     (error, results) => {
-      if (!error) {
-      let recipe = results.rows[0];
-      pool.query(
-        "select ingredient from ingredients where recipe_name = $1",
-        [title],
-        (error, results) => {
-          if (!error) {
-          let ingredientsList = [];
-          results.rows.forEach(e => ingredientsList.push(e.ingredient));
-          recipe["ingredients"] = ingredientsList;
-          response.status(200).json(recipe);
-        }
-      );
+      if (error) {
+        console.log(error)
+      } else {
+        let recipe = results.rows[0]
+        pool.query(
+          'select ingredient from ingredients where recipe_name = $1',
+          [title],
+          (error, results) => {
+            if (error) {
+              console.log(error)
+            } else {
+              let ingredientsList = []
+              results.rows.forEach(e => ingredientsList.push(e.ingredient))
+              recipe['ingredients'] = ingredientsList
+              response.status(200).json(recipe)
+            }
+          }
+        )
+      }
     }
-  );
-};
+  )
+}
 
 const getRecipesInCategory = (request, response) => {
-  const category = request.params.category;
-  const offset = (request.query.page - 1) * 15;
+  const category = request.params.category
+  const offset = (request.query.page - 1) * 15
   pool.query(
-    "select recipe_name, rating, recipe_img, prep_time from recipes where category = $1 limit 15 offset $2",
+    'select recipe_name, rating, recipe_img, prep_time from recipes where category = $1 limit 15 offset $2',
     [category, offset],
     (error, results) => {
-      if (!error) {
-      response.status(200).json(results.rows);
+      if (error) {
+        console.log(error)
+      } else {
+        response.status(200).json(results.rows)
+      }
     }
-  );
-};
+  )
+}
 
 const createRecipe = (request, response) => {
   const {
@@ -63,41 +75,47 @@ const createRecipe = (request, response) => {
     author,
     prep_time,
     ingredients
-  } = request.body;
+  } = request.body
   pool.query(
-    "insert into recipes (recipe_name, instructions, author, prep_time) values ($1, $2, $3, $4, $5)",
+    'insert into recipes (recipe_name, instructions, author, prep_time) values ($1, $2, $3, $4, $5)',
     [recipe_name, instructions, author, prep_time],
     (error, results) => {
-      if (!error) {
-      for (let i = 0; i < ingredients.length; i++) {
-        pool.query(
-          "insert into ingredients (recipe_name, ingredient, amount) values ($1, $2, $3)",
-          [recipe_name, ingredient[i]],
-          (error, results) => {
-            if (error) {
-              throw error;
+      if (error) {
+        console.log(error)
+      } else {
+        for (let i = 0; i < ingredients.length; i++) {
+          pool.query(
+            'insert into ingredients (recipe_name, ingredient, amount) values ($1, $2, $3)',
+            [recipe_name, ingredient[i]],
+            (error, results) => {
+              if (error) {
+                throw error
+              }
             }
-          }
-        );
-      }
+          )
+        }
 
-      response.status(201).send(`recipe added`);
+        response.status(201).send(`recipe added`)
+      }
     }
-  );
-};
+  )
+}
 
 const addPicture = (request, response) => {
-  const imageName = request.file.filename;
-  const recipeTitle = request.query.recipe;
+  const imageName = request.file.filename
+  const recipeTitle = request.query.recipe
   pool.query(
-    "update recipes set recipe_img = $1 where recipe_name = $2",
+    'update recipes set recipe_img = $1 where recipe_name = $2',
     [imageName, recipeTitle],
     (error, results) => {
-      if (!error) {
-      response.status(201).send("picture added");
+      if (error) {
+        console.log(error)
+      } else {
+        response.status(201).send('picture added')
+      }
     }
-  );
-};
+  )
+}
 
 module.exports = {
   getRecipes,
@@ -105,4 +123,4 @@ module.exports = {
   getRecipesInCategory,
   createRecipe,
   addPicture
-};
+}
